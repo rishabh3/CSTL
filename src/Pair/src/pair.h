0783b5306876c9efc9586a644c55b87e171d07a6
rishabh3 <rishabh97dutt@gmail.com>
2018-09-27 16:14:04 +0530

    Added pair header prototype

--- src/Pair/src/pair.h
+++ src/Pair/src/pair.h
@@ -1,4 +1,14 @@
 #ifndef _PAIR_H_
 #define _PAIR_H_
-
+#include "../util.h"
+struct pair{
+  void *first;
+  void *second;
+};
+typedef struct pair PAIR;
+PAIR* make_pair(void *first, void *second);
+void destroy_pair(PAIR*);
+void swap(PAIR*);
+bool compare(PAIR *, PAIR *);
+void modify_pair(PAIR *, void *, void *);
 #endif
---
+++ src/util.h
@@ -0,0 +1,6 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+#define TRUE 1
+#define FALSE 0
+typedef unsigned int bool
+#endif

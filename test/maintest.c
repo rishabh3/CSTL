640da745a272071ce91fd4f4a1fd006eded20c23
rishabh3 <rishabh97dutt@gmail.com>
2018-10-04 21:24:56 +0530

    Vector Modification

--- test/maintest.c
+++ test/maintest.c
@@ -17,7 +17,7 @@ int intCompare(const void* a, const void* b) {
 void list_with_ints();
 void list_with_strings();
 void list_with_floats();
- 
+
 bool iterate_int(void *data);
 bool iterate_string(void *data);
 bool iterate_float(void *data);
@@ -32,29 +32,29 @@ int main(){
   PAIR_int *myspair = make_pair_int(20, 30);
   // printf("Before Swapping First: %d, Second: %d\n", get_first_int(mypair), get_second_int(mypair));
   PRINT("TEST CASE PASSED!\n");
- 
+
   PRINT("TEST for swap\n");
   assert(get_first_int(mypair) == 10 && get_second_int(mypair) == 20);
   swap_int(mypair);
   //printf("After Swapping First: %d, Second: %d\n", get_first_int(mypair), get_second_int(mypair));
   assert(get_first_int(mypair) == 20 && get_second_int(mypair) == 10);
   PRINT("TEST CASE PASSED!\n");
- 
-  PRINT("TEST for compare\n");	
+
+  PRINT("TEST for compare\n");
   assert(compare_int(mypair, myspair) == -1);
   PRINT("TEST CASE PASSED!\n");
- 
+
   PRINT("TEST for modification\n");
   modify_pair_int(mypair, 20, 30);
   assert(get_first_int(mypair) == 20 && get_second_int(mypair) == 30);
   assert(compare_int(mypair, myspair) == 0);
   PRINT("TEST CASE PASSED!\n");
- 
+
   PRINT("TEST for destruction\n");
   destroy_pair_int(mypair);
   destroy_pair_int(myspair);
   PRINT("TEST CASE PASSED!\n");
- 
+
   PRINT("SUCCESS! ALL TEST CASES PASSED\n");
 	/*Test for Pair Ends*/
 
@@ -68,12 +68,12 @@ int main(){
   float a=4.1f,b=5.2f,c=7.2f;
   float d;int r;
   PRINT("TEST CASE PASSED!\n");
-		
+
   PRINT("TEST for push\n");
   r = stackPush(s, (void*)&a);
   assert(s->top == 0);
   assert(r == 0);
-  
+
   r = stackPush(s, (void*)&b);
   assert(s->top == 1);
   assert(r == 0);
@@ -143,18 +143,19 @@ int main(){
   r = stackDestroy(s);
   assert(r == 0);
   PRINT("TEST CASE PASSED!\n");
-	
+
   PRINT("SUCCESS! ALL TEST CASES PASSED\n");
   /*Test for Stack Ends*/
-  
+
   /*Test for List*/
   list_with_ints();
   list_with_strings();
   list_with_floats();
   PRINT("SUCCESS! ALL TEST CASES PASSED!\n");
   /*Test for List ends*/
-  return 0;
 
+  /*Test for Vector*/
+  /*PRINT("TEST for vector\n");
   int i;
   vec_int_t vec;
   vec_int_init(vec);
@@ -178,6 +179,8 @@ int main(){
 
   for (i = 0; i < vec_int_size(vec); i++) {
     printf("%d\n", vec_int_get(vec, i));
+  */
+  return 0;  
 }
 
 void list_with_ints()
@@ -189,23 +192,23 @@ void list_with_ints()
   list list;
   PRINT("TEST for int list creation\n");
   list_new(&list, sizeof(int), NULL);
- 
+
   for(int i = 1; i <= numbers; i++) {
     list_append(&list, &i);
   }
 	assert(list_size(&list) == numbers);
 	PRINT("TEST CASE PASSED!\n");
-	
+
 	PRINT("TEST for int list iteration\n");
   list_for_each(&list, iterate_int);
   PRINT("\nTEST CASE PASSED!\n");
-  
-  
+
+
   PRINT("TEST for int list destruction\n");
   list_destroy(&list);
   PRINT("TEST CASE PASSED!\n");
 }
- 
+
 void list_with_strings()
 {
 	PRINT("TEST for string list\n");
@@ -220,7 +223,7 @@ void list_with_strings()
   int i;
   list list;
   list_new(&list, sizeof(char *), free_string);
- 
+
   char *name;
   for(i = 0; i < numNames; i++) {
     //scanf("%s",elem[i]);
@@ -229,12 +232,12 @@ void list_with_strings()
   }
   assert(list_size(&list) == 5);
   PRINT("TEST CASE PASSED!\n");
- 
- 
+
+
  	PRINT("TEST for string list iteration\n");
   list_for_each(&list, iterate_string);
   PRINT("\nTEST CASE PASSED!\n");
-  
+
   PRINT("TEST for string list destruction\n");
   list_destroy(&list);
   PRINT("TEST CASE PASSED!\n");
@@ -250,7 +253,7 @@ void list_with_floats()
   PRINT("TEST for float list creation\n");
   list list;
   list_new(&list, sizeof(float), NULL);
- 
+
 	for(float i = 1.0f; i <= (float)numbers; i++) {
     list_append(&list, &i);
   }
@@ -260,19 +263,19 @@ void list_with_floats()
 	PRINT("TEST for float list iteration\n");
   list_for_each(&list, iterate_float);
   PRINT("\nTEST CASE PASSED!\n");
-  
+
   PRINT("TEST for float list destruction\n");
   list_destroy(&list);
   PRINT("TEST CASE PASSED!\n");
 }
- 
-bool iterate_int(void *data) 
+
+bool iterate_int(void *data)
 {
   //printf("Found value: %d\n", *(int *)data);
   printf("%d-> ", *(int *)data);
   return TRUE;
 }
- 
+
 bool iterate_string(void *data)
 {
  // printf("Found string value: %s\n", *(char **)data);
@@ -280,14 +283,14 @@ bool iterate_string(void *data)
   return TRUE;
 }
 
-bool iterate_float(void *data) 
+bool iterate_float(void *data)
 {
   //printf("Found value: %d\n", *(int *)data);
   printf("%f-> ", *(float *)data);
   return TRUE;
 }
- 
- 
+
+
 void free_string(void *data)
 {
   free(*(char **)data);

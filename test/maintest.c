2e568d7a3b0beedf1cc6c4258bf47d820796fed1
Nikhil Singh <singhnikhil97@gmail.com>
2018-10-04 22:35:32 +0530

    Vector Implementation

--- test/maintest.c
+++ test/maintest.c
@@ -1,7 +1,10 @@
 #include "pair.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "vec.h"
 
-int main(){
+int main()
+{
   PAIR_int *mypair = make_pair_int(10,20);
   printf("Before Swapping First: %d, Second: %d\n", get_first_int(mypair), get_second_int(mypair));
   swap_int(mypair);
@@ -9,27 +12,13 @@ int main(){
   destroy_pair_int(mypair);
   return 0;
 
-  int i;
-  vec_int_t vec;
-  vec_int_init(vec);
-  vec_int_append(vec, 2);
-  vec_int_append(vec, 100);
-  vec_int_append(vec, 101);
-  vec_int_append(vec, 102);
-  vec_int_append(vec, 103);
-  vec_int_append(vec, 104);
-  vec_int_append(vec, 105);
-  vec_int_append(vec, 106);
-  vec_int_append(vec, 107);
-  vec_int_append(vec, 108);
-  vec_int_append(vec, 109);
-  vec_int_pop(vec);
-  vec_int_append(vec, 200);
-  vec_int_pop(vec);
-  vec_int_pop(vec);
-  vec_int_del(vec, 0);
-  vec_int_del(vec, 1);
-
-  for (i = 0; i < vec_int_size(vec); i++) {
-    printf("%d\n", vec_int_get(vec, i));
+  vector(integer) x = new_vector(integer);
+  for(int i = 0; i < 314; i++)
+  {
+    x->append(x, i);}
+    for(int i = 0; i < x->size(x); i++)
+  {
+  printf("%f\n", x->get(x, i));}
+  free_vector(x);
+  return 0;
 }

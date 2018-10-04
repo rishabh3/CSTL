c3df18b6648807a6314bb94c2469ae426db23852
Nikhil Singh <singhnikhil97@gmail.com>
2018-10-04 12:40:11 +0530

    git commit

--- src/Vec/src/vec.h
+++ src/Vec/src/vec.h
@@ -36,43 +36,6 @@ typedef struct {
 
 typedef VECTOR_STRUCT VECTOR[1];
 
-static __inline void VECTOR_INIT(VECTOR vec) {
-  vec[0].alloc = 8;
-  vec[0].arr = calloc(vec->alloc, sizeof(VECTOR_TYPE));
-  vec[0].size = 0;
-}
-
-static __inline void VECTOR_APPEND(VECTOR vec, VECTOR_TYPE elt) {
-  if (vec[0].size == vec[0].alloc) {
-    vec[0].alloc += (vec[0].alloc >> 1); /* Increase size by 50% */
-    vec[0].arr = realloc(vec[0].arr, vec[0].alloc * sizeof(VECTOR_TYPE));
-  }
-
-  vec[0].arr[vec[0].size++] = elt;
-}
-
-static __inline VECTOR_TYPE VECTOR_GET(VECTOR vec, int i) {
-  return vec[0].arr[i];
-}
-
-static __inline VECTOR_TYPE VECTOR_POP(VECTOR vec) {
-  return vec[0].arr[vec[0].size--];
-}
-
-static __inline void VECTOR_DEL(VECTOR vec, int i) {
-  if (i == vec[0].size) {
-    VECTOR_POP(vec);
-  } else {
-    memmove(&(vec[0].arr[i]), &(vec[0].arr[i + 1]),
-            sizeof(VECTOR_TYPE) * vec[0].size - i - 1);
-    vec[0].size--;
-  }
-}
-
-static __inline size_t VECTOR_SIZE(VECTOR vec) {
-  return vec[0].size;
-}
-
 #undef VECTOR_CONCAT
 #undef VECTOR_MAKE_STR1
 #undef VECTOR_MAKE_STR
